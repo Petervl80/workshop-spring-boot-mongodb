cbb21145a88e72830920c9bd7775b2b8828057ae
pedro.vinicius <pedro.vinicius@educ.rec.br>
2024-10-14 17:33:24 -0300

    Obtendo um usuário por id

--- src/main/java/com/petervl80/workshopmongo/services/UserService.java
+++ src/main/java/com/petervl80/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.petervl80.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.petervl80.workshopmongo.domain.User;
 import com.petervl80.workshopmongo.repository.UserRepository;
+import com.petervl80.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
